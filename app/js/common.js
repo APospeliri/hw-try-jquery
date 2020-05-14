0cd90caa0deed56c5b6157ff1f759fdc747336e8
Irine <pospeliri@gmail.com>
2020-05-14 17:26:32 +0300

    1-3,other option for change H1

--- app/js/common.js
+++ app/js/common.js
@@ -2,9 +2,14 @@ $(function() {
 
 	// Custom JS
 	//console.log('Hello Word')
-	//first options
-	$('h1').last().text('Весенние скидки')
 
+	//first option
+	//$('h1').last().text('Весенние скидки')
+
+  //two option
 	//$('h1:eq(1)').text('Весенние скидки')
+	
+	$('#vacations-wrapper h1').text('Весенние скидки')
+
 
 });
