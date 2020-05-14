fcfa19ace7a4c2d8e346e4ff7f8fff3d676b5169
Irine <pospeliri@gmail.com>
2020-05-14 18:49:57 +0300

    add other task in one file

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,12 @@ $(function() {
 
 	// Custom JS
 	//console.log('Hello Word')
+	//1-1
+	//$('h2').css('color','purple')
+	//1-2
+	//$('.details').css('font-size','22px')
 
+	//1-3
 	//first option
 	//$('h1').last().text('Весенние скидки')
 
@@ -10,7 +15,15 @@ $(function() {
 	//$('h1:eq(1)').text('Весенние скидки')
 	
   //three option
-	 //$('#vacations-wrapper h1').text('Весенние скидки')
+	//$('#vacations-wrapper h1').text('Весенние скидки')
 
-		$('.sorting a').last().text('Africa')
+	//1-4
+	//$('span').text('$199')
+
+	//1-5 
+	//$('span').text('$199')
+	//$('.europe span').text('$99')
+
+	//1-6
+	$('.sorting a').last().text('Africa')
 });
