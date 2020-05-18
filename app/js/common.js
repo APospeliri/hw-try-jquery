00211ec08fbcfa3dd198cd5691a2b0051d003191
unknown <mashagi@yandex.ru>
2020-05-18 18:48:25 +0300

    correction: all commits united

--- app/js/common.js
+++ app/js/common.js
@@ -1,48 +1,42 @@
 $(function() {
 	// Custom JS
-<<<<<<< HEAD
-	// console.log('start')
 	console.log('Hello World')
-
-    // h2- purple
 	$("h2").css("color", "purple")
-
-    // font size 22
-	// Custom JS
 	$(".details").css("font-size", "22px")
-
-
-    // Vacation Packages changed on Spring Discount
-	// Custom JS
 	$("h1:last").text("Весенние Скидки")
-
-
-    // price changed $ 199
 	$("span").text("$199")
-
-
-    // Price for London changed on
     $("span[target='_blank']").text("$99")
-
-
-    // America chenged on USA
 	$("a").first().text("USA")
-
-
-    // Asia chenged on Africa
     $("a").last().text("Africa")
-=======
-	console.log('Hello World')
-	$("h2").css("color", "purple")
-	$(".details").css("font-size", "22px")
-	$("h1:last").text("Весенние Скидки")
-	// $(".details").text("$199")
-	$("span").text("$199")
-	$("span[target='_blank']").text("$99")
-	$("a").first().text("USA")
-	$("a").last().text("Africa")
->>>>>>> e8144c1e47c08141a774dcd8c674fb0d00b2b16c
-
-
 });
+
+// $(function() {
+	// Custom JS
+	// console.log('Hello World')
+
+	// 1-1
+	// $("h2").css("color", "purple")
+
+    // 1-2
+	// $(".details").css("font-size", "22px")
+
+
+	// 1-3 part1
+	// $("h1:last").text("Весенние Скидки")
+
+	// 1-4 part2
+	// $("span").text("$199")
+
+
+    // 1-5 part3
+    // $("span[target='_blank']").text("$99")
+
+
+    // 1-6 part1
+	// $("a").first().text("USA")
+
+
+    // 1-7 part2
+    // $("a").last().text("Africa")
+// });
 
